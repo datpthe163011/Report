a4bd2aa50f1663863e8224f6106fecae9a1671c5
datpthe163011 <datpthe163011@fpt.edu.vn>
2022-05-31 14:42:44 +0700

    •	Singleton Pattern
    •	Singleton Pattern

---
+++ Report/RP2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+namespace P01_Concept
+{
+    public sealed class Singleton
+    {
+        private Singleton() { }
+        static readonly Singleton _instance = new Singleton();
+        public static Singleton Instance => _instance;
+        private int _count = 0;
+        public void Increase() => _count++;
+        public int Count => _count;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Title = "SINGLETON DESIGN PATTERN";
+            WriteLine($"Start: {Singleton.Instance.Count}");
+            Singleton.Instance.Increase();
+            WriteLine($"First increment: {Singleton.Instance.Count}");
+            Singleton.Instance.Increase();
+            WriteLine($"Second increment: {Singleton.Instance.Count}");
+            Singleton.Instance.Increase();
+            WriteLine($"Third increment : {Singleton.Instance.Count}");
+            ReadKey();
+        }
+    }
+}
